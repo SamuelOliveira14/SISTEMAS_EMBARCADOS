dbf80694c7b022bd4ae855ac197a44e3c14ed0fc
Paulo Lopes <58816081+pauloVato-sketch@users.noreply.github.com>
2025-04-14 12:57:29 -0300

    Atualização do código da P1, consertando erros e testado na placa.

--- main/main.c
+++ main/main.c
@@ -19,6 +19,8 @@
 // Criação da TAG para facilitação de análise dos logs.
 static const char* TAG = "P1";
 
+const char* get_chip_model_name(int chip_model);
+
 // Função que funciona como main do projeto.
 void app_main(void){
     
@@ -30,16 +32,58 @@ void app_main(void){
     // Função que preenche a struct esp_chip_info_t com as informações pertinentes.
     esp_chip_info(&infos);
 
-    // Log para dizer o nome do chip e a quantidade de cores.
-    ESP_LOGI(TAG,"Modelo do chip: %s\nN° de cores: %u.", (char *)infos.model, infos.cores);
-    ESP_LOGI(TAG, "TARGET do framework: %s\nFeatures: %s%s%s%s, ",
-           CONFIG_IDF_TARGET,
+    // Logs contendo diversas informações diferentes.
+    ESP_LOGI(TAG, "Versão do framework: %s", esp_get_idf_version());
+    ESP_LOGI(TAG, "Modelo do chip: %s", get_chip_model_name(infos.model));
+    ESP_LOGI(TAG, "Nº de cores: %d", infos.cores);
+    ESP_LOGI(TAG, "Target do framework: %s", CONFIG_IDF_TARGET);
+    ESP_LOGI(TAG, "Features: %s%s%s%s, ",
            (infos.features & CHIP_FEATURE_WIFI_BGN) ? "WiFi/" : "",
            (infos.features & CHIP_FEATURE_BT) ? "BT" : "",
-           (infos.features & CHIP_FEATURE_BLE) ? "BLE" : "",
+           (infos.features & CHIP_FEATURE_BLE) ? ", BLE" : "",
            (infos.features & CHIP_FEATURE_IEEE802154) ? ", 802.15.4 (Zigbee/Thread)" : "");
+    
+    // Variavel para receber endereço MAC
+    uint8_t baseMac[6];
+  
+    // Envia o buffer acima para receber endereço MAC do Wi-Fi.
+    esp_read_mac(baseMac, ESP_MAC_WIFI_STA);
+    char mac_str[18]; // 6 bytes * 2 dígitos cada + 5 ponto-e-virgula + \0
+    
+    // Formata e salva string em um buffer mac_str
+    snprintf(mac_str, sizeof(mac_str),
+             "%02X:%02X:%02X:%02X:%02X:%02X",
+             baseMac[0], baseMac[1], baseMac[2], baseMac[3], baseMac[4], baseMac[5]);
+    ESP_LOGI(TAG, "Endereço MAC: %s", mac_str);
 
+    // Envia o buffer acima para receber endereço MAC do Bluetooth.
+    esp_read_mac(baseMac, ESP_MAC_BT);
+
+    // Formata e salva string em um buffer mac_str
+    snprintf(mac_str, sizeof(mac_str),
+            "%02X:%02X:%02X:%02X:%02X:%02X",
+            baseMac[0], baseMac[1], baseMac[2], baseMac[3], baseMac[4], baseMac[5]);
+    ESP_LOGI(TAG, "Endereço Bluetooth: %s", mac_str);
     ESP_LOGI(TAG, "Finalizando execução...");
 
     fflush(stdout);
+}
+
+
+
+// Função para pegar modelo baseado no valor carregado na struct esp_chip_info_t
+const char* get_chip_model_name(int chip_model) {
+    switch (chip_model) {
+        case 1: return "ESP32";
+        case 2: return "ESP32-S2";
+        case 4: return "ESP32-S3";
+        case 5: return "ESP32-C3";
+        case 6: return "ESP32-H2";
+        case 7: return "ESP32-C2";
+        case 8: return "ESP32-C6";
+        case 9: return "ESP32-P4";
+        case 10: return "ESP32-H4";
+        case 0:
+        default: return "UNKNOWN";
+    }
 }
